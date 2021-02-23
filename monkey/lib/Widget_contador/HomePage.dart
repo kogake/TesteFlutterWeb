7525e1e97a04823ab0e13c3710bb51511411c0fe
kekoga <kevinkoga@usp.br>
2021-02-23 13:15:10 -0300

    a

--- monkey/lib/Widget_contador/HomePage.dart
+++ monkey/lib/Widget_contador/HomePage.dart
@@ -5,23 +5,47 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+// sempre importamos a classe e o estado para criar um arquivo separado.
 class _HomePageState extends State<HomePage> {
   int counter = 0;
   // aqui ficam as declaracoes de  variaveis para utilizacao no aplicativo
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: GestureDetector(
-          child: Text('Testando $counter'),
-          onTap: () {
-            print('clicado');
-            setState(() {
-              counter++;
-              print('$counter');
-            });
-          },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('meu ERP'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.icecream),
+        onPressed: () {
+          setState(() {
+            counter++;
+            print('$counter');
+          });
+        },
+      ),
+      body: Container(
+        height: 500,
+        width: 500,
+        color: Colors.white,
+        child: Center(
+          child: Container(
+            height: 100,
+            width: 100,
+            child: Center(
+              child: GestureDetector(
+                child:
+                    Text('Testando $counter', style: TextStyle(fontSize: 20)),
+                onTap: () {
+                  setState(() {
+                    counter++;
+                    print('$counter');
+                  });
+                },
+              ),
+            ),
+          ),
         ),
       ),
     );
