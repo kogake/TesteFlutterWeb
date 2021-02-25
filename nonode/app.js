18f4c3c2e4440d32fca59bb506665509710e72db
kekoga <kevinkoga@usp.br>
2021-02-24 23:33:46 -0300

    asdasd

--- nonode/app.js
+++ nonode/app.js
@@ -14,4 +14,20 @@
 
 // app.listen(8000)
 
-console.log(firebase);
+
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onClick = () => auth.signInWithPopoup(provider);
+
+signOutBtn.onClick = () => auth.signOut();
+
